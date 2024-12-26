fdce690a728e7244a047e0c0bc105b7e686541e7
Amin Saffarnejad <vira1368@gmail.com>
2024-12-26 22:42:26 +0330

    Add Categories table.

--- FakeApis/Data/ApplicationDbContext.cs
+++ FakeApis/Data/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FakeApis.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FakeApis.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
+        public DbSet<Category> Categories { get; set; }
     }
 }
