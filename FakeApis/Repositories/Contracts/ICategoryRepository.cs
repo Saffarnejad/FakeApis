2176b0ecd88378e128b5d790ef8ffa6196b770c9
Amin Saffarnejad <vira1368@gmail.com>
2024-12-27 01:40:05 +0330

    Add ICategoryRepository.

---
+++ FakeApis/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using FakeApis.Models;
+
+namespace FakeApis.Repositories.Contracts
+{
+    public interface ICategoryRepository
+    {
+        public Task AddAsync(Category category);
+        public Task DeleteAsync(int id);
+        public Task<IEnumerable<Category>> GetAllAsync();
+        public Task<Category> GetAsync(int id);
+        public Task UpdateAsync(Category category);
+    }
+}
