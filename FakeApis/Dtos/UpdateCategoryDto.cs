ce029e4ef7eee3688028fcd2008792e329a169c8
Amin Saffarnejad <vira1368@gmail.com>
2024-12-29 22:56:12 +0330

    Add UpdateCategoryDto.

---
+++ FakeApis/Dtos/UpdateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeApis.Dtos
+{
+    public class UpdateCategoryDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
