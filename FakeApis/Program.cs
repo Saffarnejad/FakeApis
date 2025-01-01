b5aeacfcd685ff2162fd0af987753df6fe45e27b
Amin Saffarnejad <vira1368@gmail.com>
2025-01-01 23:21:38 +0330

    Add category service.

--- FakeApis/Program.cs
+++ FakeApis/Program.cs
@@ -1,7 +1,10 @@
+using FakeApis.Repositories;
+using FakeApis.Repositories.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
