1d75747eb4f6484876d8fdf61aa23399a4141917
Amin Saffarnejad <vira1368@gmail.com>
2025-01-03 00:01:18 +0330

    Add IProductRepository.

---
+++ FakeApis/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using FakeApis.Models;
+
+namespace FakeApis.Repositories.Contracts
+{
+    public interface IProductRepository
+    {
+        public Task AddAsync(Product product);
+        public Task DeleteAsync(int id);
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> GetAsync(int id);
+        public Task UpdateAsync(Product product);
+    }
+}
