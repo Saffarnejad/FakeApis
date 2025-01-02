39f1f9707ba8e631bbf591c9efae912549331aca
Amin Saffarnejad <vira1368@gmail.com>
2025-01-02 22:54:09 +0330

    Add Products table.

--- FakeApis/Data/ApplicationDbContext.cs
+++ FakeApis/Data/ApplicationDbContext.cs
@@ -6,5 +6,6 @@ namespace FakeApis.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
