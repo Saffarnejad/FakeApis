e61b93208d868a1a97cc1d406c6abbb3390dd870
Amin Saffarnejad <vira1368@gmail.com>
2025-01-02 23:22:48 +0330

    Add UpdateProductDto.

---
+++ FakeApis/Dtos/UpdateProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeApis.Dtos
+{
+    public class UpdateProductDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal? Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? CategoryId { get; set; }
+    }
+}
