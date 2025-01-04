87dc38aa4ff8d1e31d2a5d8e940f16f555903d0f
Amin Saffarnejad <vira1368@gmail.com>
2025-01-04 22:25:58 +0330

    Register ProductRepository.

--- FakeApis/Program.cs
+++ FakeApis/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
+builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
