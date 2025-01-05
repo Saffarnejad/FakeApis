414fa2b983803660d295e065886cabc0a09f0af7
Amin Saffarnejad <vira1368@gmail.com>
2025-01-05 23:49:35 +0330

    Add images table.

--- FakeApis/Data/ApplicationDbContext.cs
+++ FakeApis/Data/ApplicationDbContext.cs
@@ -6,6 +6,7 @@ namespace FakeApis.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
         public DbSet<Product> Products { get; set; }
     }
 }
