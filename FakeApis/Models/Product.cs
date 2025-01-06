0e157d5bc1bfbc2876de417459f11226a0f02b10
Amin Saffarnejad <vira1368@gmail.com>
2025-01-06 03:37:53 +0330

    Remove ImageUrl.

--- FakeApis/Models/Product.cs
+++ FakeApis/Models/Product.cs
@@ -6,10 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
-
         public ICollection<Image> Images { get; set; }
     }
 }
