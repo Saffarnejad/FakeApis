90f8659e67b1cd35b79d5c6016d0d77043499056
Amin Saffarnejad <vira1368@gmail.com>
2025-01-07 22:03:58 +0330

    Add Images in UpdateProductDto.

--- FakeApis/Dtos/UpdateProductDto.cs
+++ FakeApis/Dtos/UpdateProductDto.cs
@@ -10,7 +10,7 @@ namespace FakeApis.Dtos
         public string? Name { get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set; }
-        public string? ImageUrl { get; set; }
         public int? CategoryId { get; set; }
+        public List<IFormFile>? Images { get; set; }
     }
 }
