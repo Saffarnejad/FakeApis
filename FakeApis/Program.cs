6706a217c4a42dc78f8c7facd4f5141231c844cb
Amin Saffarnejad <vira1368@gmail.com>
2025-01-09 00:10:33 +0330

    Config Serilog.

--- FakeApis/Program.cs
+++ FakeApis/Program.cs
@@ -1,9 +1,16 @@
 using FakeApis.Repositories;
 using FakeApis.Repositories.Contracts;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+    .WriteTo.File("/log/FakeApiLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+builder.Host.UseSerilog();
+
 builder.Services.AddSingleton<ICategoryRepository, CategoryRepository>();
 builder.Services.AddSingleton<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
