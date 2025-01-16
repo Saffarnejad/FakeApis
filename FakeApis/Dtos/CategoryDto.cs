ec9a31b0200a77ea279c65d59ca6c96d9e474b50
Amin Saffarnejad <vira1368@gmail.com>
2025-01-17 00:21:16 +0330

    Revert "Name of CategortDto is required and set string length."
    This reverts commit 6b866bdfc70050033cfc0bf22434458d7f9d67c3.

--- FakeApis/Dtos/CategoryDto.cs
+++ FakeApis/Dtos/CategoryDto.cs
@@ -1,13 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FakeApis.Dtos
+﻿namespace FakeApis.Dtos
 {
     public class CategoryDto
     {
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(20)]
         public string Name { get; set; }
     }
 }
