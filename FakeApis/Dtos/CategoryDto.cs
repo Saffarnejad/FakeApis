6b866bdfc70050033cfc0bf22434458d7f9d67c3
Amin Saffarnejad <vira1368@gmail.com>
2025-01-16 23:46:23 +0330

    Name of CategortDto is required and set string length.

--- FakeApis/Dtos/CategoryDto.cs
+++ FakeApis/Dtos/CategoryDto.cs
@@ -1,8 +1,13 @@
-﻿namespace FakeApis.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FakeApis.Dtos
 {
     public class CategoryDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
     }
 }
