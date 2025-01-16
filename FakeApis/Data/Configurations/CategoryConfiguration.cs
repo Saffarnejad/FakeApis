ce3f5ed8029d3ea8c548ef80911a4bb88b8cb133
Amin Saffarnejad <vira1368@gmail.com>
2025-01-16 23:39:22 +0330

    Add CategoryConfiguration.

---
+++ FakeApis/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FakeApis.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FakeApis.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(category => category.Name).HasMaxLength(20);
+
+            var categories = new List<Category>
+            {
+                new() { Id = 1, Name = "Appliances" },
+                new() { Id = 2, Name = "Clothing" },
+                new() { Id = 3, Name = "Food" },
+                new() { Id = 4, Name = "Furniture" },
+                new() { Id = 5, Name = "Phones" }
+            };
+
+            builder.HasData(categories);
+        }
+    }
+}
