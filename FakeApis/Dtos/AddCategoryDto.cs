97d69459acadf707b192e3c96d05eaf7ca907dcd
Amin Saffarnejad <vira1368@gmail.com>
2025-01-17 00:28:59 +0330

    Set StringLength for AddCategoryDto and UpdateCategoryDto.

--- FakeApis/Dtos/AddCategoryDto.cs
+++ FakeApis/Dtos/AddCategoryDto.cs
@@ -5,6 +5,7 @@ namespace FakeApis.Dtos
     public class AddCategoryDto
     {
         [Required]
+        [StringLength(20)]
         public string Name { get; set; }
     }
 }
--- FakeApis/Dtos/UpdateCategoryDto.cs
+++ FakeApis/Dtos/UpdateCategoryDto.cs
@@ -8,6 +8,7 @@ namespace FakeApis.Dtos
         public int Id { get; set; }
 
         [Required]
+        [StringLength(20)]
         public string Name { get; set; }
     }
 }
