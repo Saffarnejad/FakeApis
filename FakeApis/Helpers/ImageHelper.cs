c2bf13e1513c8a0cff33f56f8225a471329985df
Amin Saffarnejad <vira1368@gmail.com>
2025-01-25 18:37:46 +0330

    Add ImageHelper.

---
+++ FakeApis/Helpers/ImageHelper.cs
@@ -0,0 +1,46 @@
+﻿namespace FakeApis.Helpers
+{
+    public static class ImageHelper
+    {
+        private static readonly string _defaultImageFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+
+        static ImageHelper()
+        {
+            if (!Directory.Exists(_defaultImageFolderPath))
+            {
+                Directory.CreateDirectory(_defaultImageFolderPath);
+            }
+        }
+
+        public static async Task<string> UploadImageAsync(Stream imageStream, string fileName, string folderPath = null)
+        {
+            folderPath ??= _defaultImageFolderPath;
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            var filePath = Path.Combine(folderPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await imageStream.CopyToAsync(stream);
+            }
+
+            return filePath;
+        }
+
+        public static void DeleteImage(string fileName, string folderPath = null)
+        {
+            folderPath ??= _defaultImageFolderPath;
+
+            var filePath = Path.Combine(folderPath, fileName);
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
