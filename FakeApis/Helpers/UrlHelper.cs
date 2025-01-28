c5c3b74a0610f500467ec25e22a33d2aa249b400
Amin Saffarnejad <vira1368@gmail.com>
2025-01-28 21:50:50 +0330

    Add UrlHelper class.

---
+++ FakeApis/Helpers/UrlHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace FakeApis.Helpers
+{
+    public static class UrlHelper
+    {
+        private static IHttpContextAccessor _httpContextAccessor;
+
+        public static void Configure(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public static string GetBaseUrl()
+        {
+            return $"{_httpContextAccessor.HttpContext.Request.Scheme}://{_httpContextAccessor.HttpContext.Request.Host}";
+        }
+    }
+}
