49a30028cfe45da702feef8ece750b0a422346bd
Amin Saffarnejad <vira1368@gmail.com>
2025-01-31 01:23:36 +0330

    Add ImagesController with delete image functionality
    Introduced a new ImagesController class in the FakeApis.Controllers namespace.
    The controller is decorated with [ApiController] and [Route("[controller]")] attributes.
    Added dependency injection for IImageRepository via a constructor.
    Implemented an asynchronous Delete method to handle HTTP DELETE requests for deleting an image by its ID.
    The method returns NotFound if the image is not found, and NoContent if the deletion is successful.

---
+++ FakeApis/Controllers/ImagesController.cs
@@ -0,0 +1,31 @@
+﻿using FakeApis.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeApis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var image = await _imageRepository.GetAsync(id);
+            if (image is null)
+            {
+                return NotFound();
+            }
+
+            await _imageRepository.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
