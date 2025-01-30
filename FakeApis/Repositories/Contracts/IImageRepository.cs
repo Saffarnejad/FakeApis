7d11ee417ff59b419e88d7422da6226dcbf7935d
Amin Saffarnejad <vira1368@gmail.com>
2025-01-31 01:19:42 +0330

    Add IImageRepository interface with async methods
    Introduced a new namespace `FakeApis.Repositories.Contracts` and
    added the `IImageRepository` interface. This interface includes
    two asynchronous methods: `DeleteAsync(int id)` for deleting an
    image by its ID and `GetAsync(int id)` for retrieving an image by
    its ID, which may return null if the image is not found. Also
    added a using directive for `FakeApis.Models`.

---
+++ FakeApis/Repositories/Contracts/IImageRepository.cs
@@ -0,0 +1,10 @@
+﻿using FakeApis.Models;
+
+namespace FakeApis.Repositories.Contracts
+{
+    public interface IImageRepository
+    {
+        public Task DeleteAsync(int id);
+        public Task<Image?> GetAsync(int id);
+    }
+}
