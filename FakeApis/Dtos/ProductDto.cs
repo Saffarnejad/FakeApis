baebbc864b898491c475b6aa96071c92b12ce0ff
Amin Saffarnejad <vira1368@gmail.com>
2025-01-30 23:13:36 +0330

    Refactor ProductDto Images property to use ImageDto
    Changed the type of the `Images` property in the `ProductDto` class from `List<string>` to `List<ImageDto>`. This update allows for a more structured representation of image data, potentially including additional metadata about each image.

--- FakeApis/Dtos/ProductDto.cs
+++ FakeApis/Dtos/ProductDto.cs
@@ -7,6 +7,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string CategoryName { get; set; }
-        public List<string> Images { get; set; }
+        public List<ImageDto> Images { get; set; }
     }
 }
