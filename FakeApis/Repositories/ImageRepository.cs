b238ea622200312e603131827fce50f70536eb54
Amin Saffarnejad <vira1368@gmail.com>
2025-01-31 01:21:09 +0330

    Add ImageRepository class with async methods
    Introduce ImageRepository class in FakeApis.Repositories namespace.
    This class implements IImageRepository interface and provides methods
    for interacting with image data in the database. The class includes a
    constructor that accepts an ApplicationDbContext object. Added two
    asynchronous methods: DeleteAsync(int id) for removing an image by ID
    and GetAsync(int id) for retrieving an image by ID. Included necessary
    using directives for FakeApis.Data, FakeApis.Models, FakeApis.Repositories.Contracts,
    and Microsoft.EntityFrameworkCore.

---
+++ FakeApis/Repositories/ImageRepository.cs
@@ -0,0 +1,31 @@
+﻿using FakeApis.Data;
+using FakeApis.Models;
+using FakeApis.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeApis.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ImageRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var imageInDb = await _db.Images.FirstOrDefaultAsync(i => i.Id == id);
+            if (imageInDb != null)
+            {
+                _db.Images.Remove(imageInDb);
+            }
+        }
+
+        public async Task<Image?> GetAsync(int id)
+        {
+            return await _db.Images.FirstOrDefaultAsync(i => i.Id == id);
+        }
+    }
+}
