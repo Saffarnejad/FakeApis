67baa589780ca45fe4c2d881c5dec63c06f6916d
Amin Saffarnejad <vira1368@gmail.com>
2025-02-06 23:30:25 +0330

    Add user association to Category class
    The Category class now includes a reference to the IdentityUser class from the Microsoft.AspNetCore.Identity namespace, allowing it to associate a category with a specific user. A nullable UserId property of type string was added to store the ID of the user associated with the category. Additionally, a User property of type IdentityUser was added for navigation to the IdentityUser entity, enabling access to user-related information directly from the Category class.

--- FakeApis/Models/Category.cs
+++ FakeApis/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace FakeApis.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FakeApis.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? UserId { get; set; }
+        public IdentityUser User { get; set; }
     }
 }
