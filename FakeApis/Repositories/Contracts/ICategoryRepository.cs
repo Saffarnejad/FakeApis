f44e3e6c662c41e2cd3986dd7d8ae9ea95388bc8
Amin Saffarnejad <vira1368@gmail.com>
2025-02-06 23:31:09 +0330

    Add userId parameter to ICategoryRepository methods
    The DeleteAsync, GetAllAsync, GetAsync, and UpdateAsync methods in the ICategoryRepository interface have been modified to include an additional userId parameter. This change aims to ensure that these operations are performed in the context of a specific user, enhancing security and data integrity by associating actions with user identities.

--- FakeApis/Repositories/Contracts/ICategoryRepository.cs
+++ FakeApis/Repositories/Contracts/ICategoryRepository.cs
@@ -5,9 +5,9 @@ namespace FakeApis.Repositories.Contracts
     public interface ICategoryRepository
     {
         public Task AddAsync(Category category);
-        public Task DeleteAsync(int id);
-        public Task<IEnumerable<Category>> GetAllAsync();
-        public Task<Category?> GetAsync(int id);
-        public Task UpdateAsync(Category category);
+        public Task DeleteAsync(int id, string userId);
+        public Task<IEnumerable<Category>> GetAllAsync(string userId);
+        public Task<Category?> GetAsync(int id, string userId);
+        public Task UpdateAsync(Category category, string userId);
     }
 }
