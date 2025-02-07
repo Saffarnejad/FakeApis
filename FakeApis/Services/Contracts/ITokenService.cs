7c7d2a63d9b268880a17b87dbf32b6c2bb1faf60
Amin Saffarnejad <vira1368@gmail.com>
2025-02-07 04:03:05 +0330

    Add ITokenService interface with GenerateToken method
    Introduced a new ITokenService interface in the FakeApis.Services.Contracts namespace. This interface includes a GenerateToken method that takes an IdentityUser object and returns a string. Added a using directive for Microsoft.AspNetCore.Identity to support IdentityUser.

---
+++ FakeApis/Services/Contracts/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FakeApis.Services.Contracts
+{
+    public interface ITokenService
+    {
+        string GenerateToken(IdentityUser user);
+    }
+}
