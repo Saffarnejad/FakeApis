5b554262874469bd72c79cc56573c6955fd33de3
Amin Saffarnejad <vira1368@gmail.com>
2025-02-11 00:26:04 +0330

    Add HeaderMiddleware to handle JWT token in HTTP requests
    Introduce HeaderMiddleware in FakeApis.Middlewares to check for
    an "Authorization" header in incoming HTTP requests. Extract
    JWT token, retrieve NameIdentifier and sub claims, and add
    them to HttpContext.Items as "UserId" and "UserName". Call
    the next delegate in the pipeline. Add necessary using
    directives for JWT handling.

---
+++ FakeApis/Middlewares/HeaderMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace FakeApis.Middlewares
+{
+    public class HeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.Request.Headers.ContainsKey("Authorization"))
+            {
+                var token = context.Request.Headers["Authorization"].ToString().Split(' ').Last();
+                var tokenHandler = new JwtSecurityTokenHandler();
+
+                if (tokenHandler.ReadToken(token) is JwtSecurityToken jwtToken)
+                {
+                    var userId = jwtToken.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).Value;
+                    context.Items.Add("UserId", userId);
+                    var userName = jwtToken.Claims.First(claim => claim.Type == "sub").Value;
+                    context.Items.Add("UserName", userName);
+                }
+            }
+            await _next(context);
+        }
+    }
+}
