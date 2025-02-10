27ce19bdefaacd09cda7ac717045c6ac39f7a40e
Amin Saffarnejad <vira1368@gmail.com>
2025-02-10 21:25:11 +0330

    Refactor repository methods to return Task<bool>
    Modified DeleteAsync and UpdateAsync methods in ICategoryRepository
    to return Task<bool> instead of Task, indicating the success or
    failure of the operations.

--- FakeApis/Repositories/Contracts/ICategoryRepository.cs
+++ FakeApis/Repositories/Contracts/ICategoryRepository.cs
@@ -5,9 +5,9 @@ namespace FakeApis.Repositories.Contracts
     public interface ICategoryRepository
     {
         public Task AddAsync(Category category);
-        public Task DeleteAsync(int id, string userId);
+        public Task<bool> DeleteAsync(int id, string userId);
         public Task<IEnumerable<Category>> GetAllAsync(string userId);
         public Task<Category?> GetAsync(int id, string userId);
-        public Task UpdateAsync(Category category, string userId);
+        public Task<bool> UpdateAsync(Category category, string userId);
     }
 }
