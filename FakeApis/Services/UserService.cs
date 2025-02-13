cccf9a2d727c4701863857a87a76c3a404dbfa23
Amin Saffarnejad <vira1368@gmail.com>
2025-02-13 22:33:05 +0330

    Refactor UserService to use IHttpContextAccessor
    Added a new UserService class that implements the IUserService interface within the FakeApis.Services namespace. Injected IHttpContextAccessor to access HTTP context. Implemented GetUserId method to retrieve the user ID from claims in the HTTP context using ClaimTypes.NameIdentifier. Commented out the previous implementation of GetUserId that used HttpContext.Items collection.

---
+++ FakeApis/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using FakeApis.Services.Contracts;
+using System.Security.Claims;
+
+namespace FakeApis.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? GetUserId()
+        {
+            //var userId = _httpContextAccessor.HttpContext?.Items.FirstOrDefault(item => item.Key == "UserId").Value?.ToString();
+            var userId = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            return userId;
+        }
+    }
+}
