096b0eb81690a5ec309e9f98e7c06141becafa71
Amin Saffarnejad <vira1368@gmail.com>
2025-02-20 00:11:39 +0330

    Add console logging to existing file logging setup
    Updated the logging configuration in `Program.cs` to include console logging in addition to the existing file logging. Logs will now be written both to the console and to the file at `/log/FakeApiLogs.txt` with a daily rolling interval. This change facilitates real-time log monitoring during development and debugging.

--- FakeApis/Program.cs
+++ FakeApis/Program.cs
@@ -18,8 +18,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
-    .WriteTo.File("/log/FakeApiLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("/log/FakeApiLogs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 builder.Host.UseSerilog();
 
