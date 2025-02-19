35f00540ab9379a6142ac84c850162103be9ae38
Amin Saffarnejad <vira1368@gmail.com>
2025-02-19 23:43:26 +0330

    Add extension method for ExceptionHandlerMiddleware
    Introduce FakeApis.Middlewares namespace with MiddlewareExtensions class.
    Add UseExceptionHandlerMiddleware extension method to IApplicationBuilder
    to simplify adding ExceptionHandlerMiddleware to the pipeline.

---
+++ FakeApis/Middlewares/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace FakeApis.Middlewares
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandlerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
