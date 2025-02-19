82dd5a9dcdb57dc44ec0b3737e4301867166a8b9
Amin Saffarnejad <vira1368@gmail.com>
2025-02-19 23:38:47 +0330

    Add BadRequestException class in FakeApis.Exceptions
    Introduced a new namespace `FakeApis.Exceptions` and added a
    custom exception class `BadRequestException` within it. This
    class inherits from the base `Exception` class and includes a
    constructor that accepts a string message, which is passed to
    the base class constructor.

---
+++ FakeApis/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace FakeApis.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
